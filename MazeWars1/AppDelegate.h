f5f8cf962adf2fe92ed8ba4f197402c8c6d0065d
Tito Babatunde <taliax023@gmail.com>
2022-12-06 22:48:41 -0500

    Initial Commit

---
+++ MazeWars1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MazeWars1
+//
+//  Created by Tito Babatunde on 12/6/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
